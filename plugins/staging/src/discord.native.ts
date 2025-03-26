a7ce4717fb6fe48ca766afde68316520f58e19fb
wont-stream <143244075+wont-stream@users.noreply.github.com>
2025-03-25 23:56:54 -0400

    test?

---
+++ plugins/staging/src/discord.native.ts
@@ -0,0 +1,23 @@
+import electron from "electron";
+
+const getCurrent = () => {
+	alert(electron.autoUpdater.getFeedURL());
+}
+
+const setBeta = (beta: boolean) => {
+	electron.autoUpdater.setFeedURL({
+		url: electron.autoUpdater.getFeedURL(),
+	});
+	if (beta) {
+		electron.autoUpdater.setFeedURL({
+			url: electron.autoUpdater.getFeedURL() + "/beta",
+		});
+	}
+}
+
+electron.ipcMain.removeHandler("UPDATE_GET_CURRENT");
+electron.ipcMain.removeHandler("UPDATE_SET_BETA");
+electron.ipcMain.handle("UPDATE_GET_CURRENT", getCurrent);
+electron.ipcMain.handle("UPDATE_SET_BETA", (event, beta: boolean) => {
+	setBeta(beta);
+});
